f6f0fd12e929657d979da1518e21317e247d0f1a
Yassine Hanini <yassine.hanini@almaviacx.com>
2023-02-17 14:44:41 +0100

    init bundle

---
+++ src/AlmaviacxRestrictedSiteaccessBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * AlmaviacxRestrictedSiteaccessBundle.
+ *
+ * @package   AlmaviaCX\RestrictedSiteaccess
+ *
+ * @author    AlmaviaCX
+ * @copyright 2023 AlmaviaCX
+ * @license   https://github.com/Novactive/AlmaviaCXRestrictedSiteAccessBundle/blob/main/LICENSE MIT Licence
+ */
+
+declare(strict_types=1);
+
+namespace AlmaviaCX\RestrictedSiteaccess;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class AlmaviacxRestrictedSiteaccessBundle extends AbstractBundle
+{
+
+}
